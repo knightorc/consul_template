d446f611f6d93b9533d86b1bdccb466a76cf89a6
Riley Shott <rileyshott+git@gmail.com>
2016-04-05 17:12:18 -0700

    Fix travis-ci

--- test/spec/libraries/consul_template_service_windows_spec.rb
+++ test/spec/libraries/consul_template_service_windows_spec.rb
@@ -4,6 +4,18 @@ require_relative '../../../libraries/consul_template_service'
 describe ConsulTemplateCookbook::Resource::ConsulTemplateService do
   step_into(:consul_template_service)
   let(:chefspec_options) { { platform: 'windows', version: '2012R2' } }
+  let(:shellout) { double('shellout') }
+
+  before do
+    allow(Mixlib::ShellOut).to receive(:new).and_return(shellout)
+    allow(shellout).to receive(:live_stream)
+    allow(shellout).to receive(:live_stream=)
+    allow(shellout).to receive(:error!)
+    allow(shellout).to receive(:stderr)
+    allow(shellout).to receive(:run_command)
+    allow(shellout).to receive(:exitstatus).and_return(0)
+    allow(shellout).to receive(:stdout).and_return("consul-template v0.14.0\n")
+  end
 
   recipe 'consul_template::default'
 
